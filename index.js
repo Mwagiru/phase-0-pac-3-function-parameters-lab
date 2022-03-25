924ce262c2cba43ec3398195e12dbb0fa224c34b
Mwagiru <joymwagiru@gmail.com>
2022-03-25 14:33:05 +0300

    Complete assignment

---
+++ index.js
@@ -0,0 +1,4 @@
+function introductionWithLanguageOPtional(name , language="JavaScript"){
+    console.log(`Hello, ${name} fav language is ${language}`);
+}
+introductionWithLanguageOPtional("Mwagiru" , );
